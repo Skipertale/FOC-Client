280b2358c91408fe6ee6743e0fe48c8c1ac573e8
Skipertale <skipertale@gmail.com>
2026-07-25 16:04:09 +0300

    FOC client
    16 channels, URL SFX, logo_foc, ambience fix

--- Start - Windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:while
-echo Starting up server...
-py start_server.py
-echo Server has shut down, restarting...
-timeout /t 2 /nobreak
-goto :while
--- client/updatescript/update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist .\updatescript.ps1 (
-    powershell.exe -executionpolicy bypass .\updatescript.ps1 -prerelease -autostart
-) else (
-    echo Error: updatescript.ps1 is missing! Cannot proceed with automatic updates.
-    pause
-)
